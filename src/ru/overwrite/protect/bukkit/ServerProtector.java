64c7c80f077438b25b8e51687d3a26c4029cb3a4
Overwrite987 <92237090+Overwrite987@users.noreply.github.com>
2023-02-07 06:34:35 +0300

    123

---
+++ src/ru/overwrite/protect/bukkit/ServerProtector.java
@@ -0,0 +1,57 @@
+package ru.overwrite.protect.bukkit;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import ru.overwrite.protect.bukkit.utils.Config;
+import ru.overwrite.protect.bukkit.utils.Metrics;
+
+import java.util.Date;
+
+public final class ServerProtector extends ServerProtectorManager {
+    public final PasswordHandler passwordHandler = new PasswordHandler(this);
+    
+    private static ServerProtector instance;
+
+    public static ServerProtector getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onEnable() {
+        checkPaper();
+        long startTime = System.currentTimeMillis();
+        Date date = new Date(startTime);
+        instance = this;
+        saveConfigs();
+        reloadConfigs();
+        registerListeners();
+        registerCommands(this);
+        startRunners();
+        logEnableDisable(message.getString("log-format.enabled"), date);
+        if (getConfig().getBoolean("main-settings.enable-metrics")) {
+            new Metrics(this, 13347);
+        }
+        checkForUpdates();
+        long endTime = System.currentTimeMillis();
+        getLogger().info("Plugin started in " + (endTime - startTime) + " ms");
+    }
+
+    @Override
+    public void onDisable() {
+        Date date = new Date();
+        FileConfiguration message = Config.getFile("message.yml");;
+        getServer().getScheduler().cancelTasks(this);
+        instance = null;
+        login.clear();
+        ips.clear();
+        time.clear();
+        passwordHandler.clearAttempts();
+        logEnableDisable(message.getString("log-format.disabled"), date);
+        if (getConfig().getBoolean("message-settings.enable-broadcasts")) {
+        	getServer().broadcast(getMessage("broadcasts.disabled"), "serverprotector.admin");
+        }
+        if (getConfig().getBoolean("secure-settings.shutdown-on-disable")) {
+        	getServer().shutdown();
+        }
+    }
+}
+  
---
+++ src/ru/overwrite/protect/bukkit/utils/Config.java
@@ -0,0 +1,50 @@
+package ru.overwrite.protect.bukkit.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import ru.overwrite.protect.bukkit.ServerProtector;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Config {
+	
+	private static final ServerProtector instance = ServerProtector.getInstance();
+
+	public static FileConfiguration getFile(String fileName) {
+	    File file = new File(ServerProtector.getInstance().getDataFolder(), fileName);
+	    if (!file.exists()) {
+	    	instance.saveResource(fileName, false);
+	    }
+	    return YamlConfiguration.loadConfiguration(file);
+	}
+    
+    public static FileConfiguration getFileFullPath(String fileName) {
+        File file = new File(instance.getConfig().getString("file-settings.data-file-path"), fileName);
+        if (!file.exists()) {
+        	instance.saveResource(fileName, false);
+        }
+        return YamlConfiguration.loadConfiguration(file);
+    }
+
+    public static void save(FileConfiguration config, String fileName) {
+        Bukkit.getScheduler().runTaskAsynchronously(instance, () -> {
+            try {
+                config.save(new File(instance.getDataFolder(), fileName));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+    	});
+    }
+    
+    public static void saveFullPath(FileConfiguration config, String fileName) {
+        Bukkit.getScheduler().runTaskAsynchronously(ServerProtector.getInstance(), () -> {
+            try {
+             config.save(new File(instance.getConfig().getString("file-settings.data-file-path"), fileName));
+            } catch (IOException ex) {
+               ex.printStackTrace();
+            }
+        });
+    }
+}
