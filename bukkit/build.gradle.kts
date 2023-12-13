a9e2381c0470c6bb23bdec9d8d96c5d04714ac73
Roman Makeev <makeevrserg@gmail.com>
2023-12-13 08:43:51 +0300

    add ci

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -42,11 +42,11 @@ listOf("en", "ru").forEach { lang ->
         dependsOn(configurations)
         archiveClassifier.set(null as String?)
         relocate("org.bstats", projectInfo.group)
-        // Skip version as ci not supports it
-        archiveVersion.set("")
+        archiveVersion.set(projectInfo.versionString)
         archiveBaseName.set(projectInfo.name + "-$lang")
     }
 }
+
 // required for backwards compatibility of old versions
 afterEvaluate {
     rootProject.file("VERSION").writeText(projectInfo.versionString)
