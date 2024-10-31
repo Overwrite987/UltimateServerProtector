eab43a77e5a58c43808c0db6dddb1d81ba79cbd3
Overwrite987 <artem.korolev_2003@mail.ru>
2024-10-31 08:40:46 +0300

    improve even more

--- bukkit/src/main/java/ru/overwrite/protect/bukkit/commands/subcommands/UpdateSubcommand.java
+++ bukkit/src/main/java/ru/overwrite/protect/bukkit/commands/subcommands/UpdateSubcommand.java
@@ -8,8 +8,6 @@ import ru.overwrite.protect.bukkit.utils.Utils;
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 
 public class UpdateSubcommand extends AbstractSubCommand {
 
@@ -24,32 +22,30 @@ public class UpdateSubcommand extends AbstractSubCommand {
     }
 
     public void checkAndUpdatePlugin(CommandSender sender, ServerProtectorManager plugin) {
-        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-            Utils.checkUpdates(plugin, version -> {
-                sender.sendMessage(plugin.messageFile.getString("system.baseline-default", "§6========================================"));
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> Utils.checkUpdates(plugin, version -> {
+            sender.sendMessage(plugin.messageFile.getString("system.baseline-default", "§6========================================"));
 
-                String currentVersion = plugin.getDescription().getVersion();
+            String currentVersion = plugin.getDescription().getVersion();
 
-                if (currentVersion.equals(version)) {
-                    sender.sendMessage(plugin.messageFile.getString("system.update-latest", "§aYou are using latest version of the plugin!"));
-                } else {
-                    String currentJarName = new File(plugin.getClass().getProtectionDomain().getCodeSource().getLocation().getPath()).getName();
-                    String downloadUrl = "https://github.com/Overwrite987/UltimateServerProtector/releases/download/" + version + "/" + "UltimateServerProtector.jar";
-                    try {
-                        File updateFolder = Bukkit.getUpdateFolderFile();
-                        File targetFile = new File(updateFolder, currentJarName);
+            if (currentVersion.equals(version)) {
+                sender.sendMessage(plugin.messageFile.getString("system.update-latest", "§aYou are using latest version of the plugin!"));
+            } else {
+                String currentJarName = new File(plugin.getClass().getProtectionDomain().getCodeSource().getLocation().getPath()).getName();
+                String downloadUrl = "https://github.com/Overwrite987/UltimateServerProtector/releases/download/" + version + "/" + "UltimateServerProtector.jar";
+                try {
+                    File updateFolder = Bukkit.getUpdateFolderFile();
+                    File targetFile = new File(updateFolder, currentJarName);
 
-                        downloadFile(downloadUrl, targetFile, sender);
+                    downloadFile(downloadUrl, targetFile, sender);
 
-                        sender.sendMessage(plugin.messageFile.getString("system.update-success-1", "§aUpdate was downloaded successfully!"));
-                        sender.sendMessage(plugin.messageFile.getString("system.update-success-2", "§aRestart the server to apply the update."));
-                    } catch (IOException ex) {
-                        sender.sendMessage("Unable to download update: " + ex.getMessage());
-                    }
+                    sender.sendMessage(plugin.messageFile.getString("system.update-success-1", "§aUpdate was downloaded successfully!"));
+                    sender.sendMessage(plugin.messageFile.getString("system.update-success-2", "§aRestart the server to apply the update."));
+                } catch (IOException ex) {
+                    sender.sendMessage("Unable to download update: " + ex.getMessage());
                 }
-                sender.sendMessage(plugin.messageFile.getString("system.baseline-default", "§6========================================"));
-            });
-        });
+            }
+            sender.sendMessage(plugin.messageFile.getString("system.baseline-default", "§6========================================"));
+        }));
     }
 
     public void downloadFile(String fileURL, File targetFile, CommandSender sender) throws IOException {
