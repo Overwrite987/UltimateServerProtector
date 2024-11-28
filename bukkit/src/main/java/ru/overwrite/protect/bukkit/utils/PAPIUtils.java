77ade089d1db4f7ab6cd21f6a983e5839e4084a7
Overwrite987 <artem.korolev_2003@mail.ru>
2024-11-28 04:05:13 +0300

    finalize util classes

--- bukkit/src/main/java/ru/overwrite/protect/bukkit/utils/PAPIUtils.java
+++ bukkit/src/main/java/ru/overwrite/protect/bukkit/utils/PAPIUtils.java
@@ -3,7 +3,7 @@ package ru.overwrite.protect.bukkit.utils;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.entity.Player;
 
-public class PAPIUtils {
+public final class PAPIUtils {
 
     public static String parsePlaceholders(Player p, String message) {
         return Utils.COLORIZER.colorize(PlaceholderAPI.setPlaceholders(p, message));
--- bukkit/src/main/java/ru/overwrite/protect/bukkit/utils/PluginMessage.java
+++ bukkit/src/main/java/ru/overwrite/protect/bukkit/utils/PluginMessage.java
@@ -11,7 +11,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
-public class PluginMessage implements PluginMessageListener {
+public final class PluginMessage implements PluginMessageListener {
 
     private final ServerProtectorManager plugin;
 
