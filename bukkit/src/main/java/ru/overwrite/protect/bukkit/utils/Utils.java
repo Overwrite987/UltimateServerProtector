41bea2f7ecbc9e86b0f6b08cc1add18cc9b3c680
Overwrite987 <artem.korolev_2003@mail.ru>
2025-04-20 18:43:40 +0300

    improve valid color character check

--- bukkit/src/main/java/ru/overwrite/protect/bukkit/utils/Utils.java
+++ bukkit/src/main/java/ru/overwrite/protect/bukkit/utils/Utils.java
@@ -101,15 +101,11 @@ public class Utils {
     }
 
     private boolean isValidColorCharacter(char c) {
-        return (c >= '0' && c <= '9') ||
-                (c >= 'a' && c <= 'f') ||
-                c == 'r' ||
-                (c >= 'k' && c <= 'o') ||
-                c == 'x' ||
-                (c >= 'A' && c <= 'F') ||
-                c == 'R' ||
-                (c >= 'K' && c <= 'O') ||
-                c == 'X';
+        return switch (c) {
+            case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'A', 'B', 'C', 'D',
+                 'E', 'F', 'r', 'R', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'x', 'X' -> true;
+            default -> false;
+        };
     }
 
     public void checkUpdates(ServerProtectorManager plugin, Consumer<String> consumer) {
--- bukkit/src/main/java/ru/overwrite/protect/bukkit/utils/color/LegacyAdvancedColorizer.java
+++ bukkit/src/main/java/ru/overwrite/protect/bukkit/utils/color/LegacyAdvancedColorizer.java
@@ -94,8 +94,11 @@ public class LegacyAdvancedColorizer implements Colorizer {
     }
 
     private boolean isValidColorCharacter(char c) {
-        return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || c == 'r' || (c >= 'k' && c <= 'o') ||
-                (c >= 'A' && c <= 'F') || c == 'R' || (c >= 'K' && c <= 'O');
+        return switch (c) {
+            case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'A', 'B', 'C', 'D',
+                 'E', 'F', 'r', 'R', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O' -> true;
+            default -> false;
+        };
     }
 
     private void appendRemainingColorTags(StringBuilder builder, boolean isColor, boolean isHashtag, boolean isDoubleTag) {
