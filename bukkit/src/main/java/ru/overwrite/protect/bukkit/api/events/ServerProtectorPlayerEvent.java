955281de66293efe0c718cd5f56498a9a1da3350
Overwrite987 <artem.korolev_2003@mail.ru>
2025-06-25 05:41:16 +0300

    once again copy paper api since spigot sucks

--- bukkit/src/main/java/ru/overwrite/protect/bukkit/api/events/ServerProtectorPlayerEvent.java
+++ bukkit/src/main/java/ru/overwrite/protect/bukkit/api/events/ServerProtectorPlayerEvent.java
@@ -1,6 +1,7 @@
 package ru.overwrite.protect.bukkit.api.events;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,6 +17,21 @@ public abstract class ServerProtectorPlayerEvent extends Event {
     public ServerProtectorPlayerEvent(@NotNull final Player who, boolean async) {
         super(async);
         this.player = who;
+    }
+
+    /**
+     * Calls the event and tests if cancelled.
+     *
+     * @return false if event was cancelled, if cancellable. otherwise true.
+     */
+    @Override
+    public boolean callEvent() {
+        org.bukkit.Bukkit.getPluginManager().callEvent(this);
+        if (this instanceof Cancellable) {
+            return !((Cancellable) this).isCancelled();
+        } else {
+            return true;
+        }
     }
 
     /**
