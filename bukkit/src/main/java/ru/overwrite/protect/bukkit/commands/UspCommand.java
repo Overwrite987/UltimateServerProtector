f631298bc2e7f72c12451eeecc83e2d46586c18a
Overwrite987 <artem.korolev_2003@mail.ru>
2025-07-10 18:25:57 +0300

    make it like that

--- bukkit/src/main/java/ru/overwrite/protect/bukkit/commands/UspCommand.java
+++ bukkit/src/main/java/ru/overwrite/protect/bukkit/commands/UspCommand.java
@@ -21,7 +21,7 @@ public final class UspCommand implements TabExecutor {
     private final ServerProtectorManager plugin;
     private final Config pluginConfig;
 
-    private final Map<String, AbstractSubCommand> subCommands = new HashMap<>();
+    private final Map<String, AbstractSubCommand> subCommands = new HashMap<>(16, 1.0f);
 
     public UspCommand(ServerProtectorManager plugin) {
         this.plugin = plugin;
