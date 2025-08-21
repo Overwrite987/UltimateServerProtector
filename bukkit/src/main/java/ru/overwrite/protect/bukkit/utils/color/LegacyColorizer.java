3711514e51e5774a613ba844dcf8ca0c6e949fd6
Overwrite987 <artem.korolev_2003@mail.ru>
2025-08-21 06:30:54 +0300

    ladies and gentelmen. we saved almost 50 nanosec.

--- bukkit/src/main/java/ru/overwrite/protect/bukkit/utils/color/LegacyColorizer.java
+++ bukkit/src/main/java/ru/overwrite/protect/bukkit/utils/color/LegacyColorizer.java
@@ -17,15 +17,15 @@ public class LegacyColorizer implements Colorizer {
         final Matcher matcher = HEX_PATTERN.matcher(message);
         final StringBuilder builder = new StringBuilder(message.length() + 32);
         while (matcher.find()) {
-            String group = matcher.group(1);
+            char[] group = matcher.group(1).toCharArray();
             matcher.appendReplacement(builder,
                     Utils.COLOR_CHAR + "x" +
-                            Utils.COLOR_CHAR + group.charAt(0) +
-                            Utils.COLOR_CHAR + group.charAt(1) +
-                            Utils.COLOR_CHAR + group.charAt(2) +
-                            Utils.COLOR_CHAR + group.charAt(3) +
-                            Utils.COLOR_CHAR + group.charAt(4) +
-                            Utils.COLOR_CHAR + group.charAt(5));
+                            Utils.COLOR_CHAR + group[0] +
+                            Utils.COLOR_CHAR + group[1] +
+                            Utils.COLOR_CHAR + group[2] +
+                            Utils.COLOR_CHAR + group[3] +
+                            Utils.COLOR_CHAR + group[4] +
+                            Utils.COLOR_CHAR + group[5]);
         }
         message = matcher.appendTail(builder).toString();
         return Utils.translateAlternateColorCodes('&', message);
